211fb44847cf90a0bb4c15b7106a45173059e58d
Toni Ahola <aholatmj@gmail.com>
2012-10-23 20:48:56 +0300

    Added db.sql

---
+++ backend/db/db.sql
@@ -0,0 +1,20 @@
+create table team (
+	id int unsigned not null auto_increment, 
+	name varchar(255),
+	primary key(id)
+) default charset UTF-8;
+
+create table person (
+	id int unsigned not null auto_increment,
+	name varchar(255),
+	primary key(id)
+)
+
+create table person_mood (
+	id int unsigned not null auto_increment,
+	time datetime,
+	mood int,
+	pid int,
+	primary key(id),
+	foreign key(pid) references person(id) on delete restrict
+)
