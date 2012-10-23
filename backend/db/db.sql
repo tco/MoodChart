656e311afbf578a25b210c92500e75f6f035a518
Toni Ahola <aholatmj@gmail.com>
2012-10-23 21:12:22 +0300

    team_person mapping

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -1,22 +1,29 @@
 create table team (
 	id int unsigned not null auto_increment, 
-	name varchar(255),
+	name varchar(255) not null,
 	primary key(id)
-) default charset UTF-8;
+) default charset utf8;
 
 create table person (
 	id int unsigned not null auto_increment,
-	name varchar(255),
-	email varchar(255),
-	password varchar(64), 
+	name varchar(255) not null,
+	email varchar(255) not null,
+	password varchar(64) not null, 
 	primary key(id)
-) default charset UTF-8;
+) default charset utf8;
+
+create table team_person (
+	tid unsigned not null,
+	pid unsigned not null,
+	foreign key(tid) referenced team(id) on delete restrict,
+	foreign key(pid) referenced person(id) on delete restrict,
+)
 
 create table person_mood (
 	id int unsigned not null auto_increment,
-	time datetime,
-	mood int,
-	pid int,
+	time datetime not null,
+	mood int unsigned not null,
+	pid int unsigned not null,
 	primary key(id),
 	foreign key(pid) references person(id) on delete restrict
-) default charset UTF-8;
+) default charset utf8;
